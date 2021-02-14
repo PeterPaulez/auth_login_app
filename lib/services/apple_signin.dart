c2cf18677add8c3a482d9bb15d4be46dee42aaf8
Pedro Pablo Aparicio <pedroapariciocristobal@gmail.com>
2021-02-14 13:55:06 +0100

    CHANGE: Conectamos nuestro backend con nuestro login de Apple para que se mande toda la info obtenida.

--- lib/services/apple_signin.dart
+++ lib/services/apple_signin.dart
@@ -1,15 +1,39 @@
+import 'dart:io';
+import 'package:http/http.dart' as http;
 import 'package:sign_in_with_apple/sign_in_with_apple.dart';
 
 class AppleSigninService {
   static void singIn() async {
-    final credential = await SignInWithApple.getAppleIDCredential(
-      scopes: [
-        AppleIDAuthorizationScopes.email,
-        AppleIDAuthorizationScopes.fullName,
-      ],
-    );
+    try {
+      final credential = await SignInWithApple.getAppleIDCredential(
+        scopes: [
+          AppleIDAuthorizationScopes.email,
+          AppleIDAuthorizationScopes.fullName,
+        ],
+      );
 
-    print(credential);
-    print(credential.authorizationCode);
+      print(credential);
+      print(credential.authorizationCode);
+      final signInAppleURL = Uri(
+          scheme: 'https',
+          host: 'flutter-google-singin.herokuapp.com',
+          path: '/api/usuario/apple/signin',
+          queryParameters: {
+            'code': credential.authorizationCode,
+            'firstName': credential.givenName,
+            'lastName': credential.familyName,
+            'useBundleId': Platform.isIOS ? 'true' : 'false',
+            if (credential.state != null) 'state': credential.state
+          });
+
+      final session = await http.post(signInAppleURL);
+
+      print('====== BACKEND ======');
+      print(session.body);
+    } catch (e) {
+      print('Error en Apple');
+      print(e);
+      return null;
+    }
   }
 }
